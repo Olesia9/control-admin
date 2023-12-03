c0b73b7c6cd0390654b1476f7757c7d2a8375d1f
olesi <olesia.zamar@gmail.com>
2023-12-03 11:18:59 +0600

    удаление ненужных комментариев

--- api/config/database.php
+++ api/config/database.php
@@ -1,34 +1,8 @@
 <?php
-//
-//class Database
-//{
-//    // укажите свои учетные данные базы данных
-//    private $host = "127.0.0.1";
-//    private $db_name = "api_goods_service_db";
-//    private $username = "root";
-//    private $password = "";
-//    public $conn;
-//
-//    // получаем соединение с БД
-//    public function getConnection()
-//    {
-//        $this->conn = null;
-//
-//        try {
-//            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-//            $this->conn->exec("set names utf8");
-//        } catch (PDOException $exception) {
-//            echo "Ошибка подключения: " . $exception->getMessage();
-//        }
-//
-//        return $this->conn;
-//    }
-//}
-
 
 class Database
 {
-    // укажите свои учетные данные базы данных
+    // учетные данные базы данных
     private $host = "127.0.0.1";
     private $db_name = "med_admin_db";
     private $username = "root";
